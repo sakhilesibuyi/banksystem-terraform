7680b0fe2aa8ffa71ff82c5b9a314ea97aadd668
sakhilesibuyi1 <sakhile.sibuyi@ayoba.me>
2022-06-30 10:23:31 +0200

    re add tags

--- main.tf
+++ main.tf
@@ -15,5 +15,8 @@ provider "aws" {
 resource "aws_instance" "ec2_instance" {
     ami = "ami-0022f774911c1d690"
     instance_type = "t2.micro"
+    tags = {
+        name ="ExampleEC2InstanceGIT"
+    }
   
 }
